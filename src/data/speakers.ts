23eec941ea376e4a298d99fb26819f0a77d1c86e
Felix Alejandro Zelaya Orellana <fazogato@gmail.com>
2024-03-23 17:44:27 -0400

    9 - add speakers section and data, and modify some styling

---
+++ src/data/speakers.ts
@@ -0,0 +1,263 @@
+import CamilaVia from "../assets/images/speakers/camila-via.png";
+import FlaviaDavila from "../assets/images/speakers/flavia-davila.png";
+import NatalyGemio from "../assets/images/speakers/nataly-gemio.png";
+import GabrielaTorrico from "../assets/images/speakers/gabriela-torrico.png";
+import AdrianaPaz from "../assets/images/speakers/adriana-paz.png";
+import JacquelineMejia from "../assets/images/speakers/jacqueline-mejia.png";
+import NataliaAlfaro from "../assets/images/speakers/natalia-alfaro.png";
+import MariaGarcia from "../assets/images/speakers/maria-garcia.png";
+import FrancisBanos from "../assets/images/speakers/nicole-banos.png";
+import LupeMelany from "../assets/images/speakers/lupe-mel.png";
+import PedroAnze from "../assets/images/speakers/pedro-anze.png";
+import CarliCode from "../assets/images/speakers/carla-florida.png";
+import MauricioArce from "../assets/images/speakers/mauricio-arce.png";
+import ReneCardozo from "../assets/images/speakers/rene-cardozo.png";
+import ElynMamani from "../assets/images/speakers/elyn-mamani.png";
+
+import logoWeb from "../assets/images/social/web.svg";
+import logoLinkedin from "../assets/images/social/linkedin.svg";
+import logoFacebook from "../assets/images/social/facebook.svg";
+import logoInstagram from "../assets/images/social/instagram.svg";
+import logoTwitter from "../assets/images/social/twitter.svg";
+import logoGithub from "../assets/images/social/github.svg";
+import logoYoutube from "../assets/images/social/youtube.svg";
+
+export const speakers = [
+  {
+    img: CamilaVia,
+    name: "Camila Flor Via",
+    bio: "CEO de Podcast Agarrate Catalina",
+    social: [
+      {
+        social_img: logoFacebook,
+        src: "https://www.facebook.com/Agarratecatalinabol",
+      },
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/agarratecatalinabol?igsh=ZmU1ajI4MXFhOW0z",
+      },
+    ],
+  },
+  {
+    img: FlaviaDavila,
+    name: "Flavia Maylin Davila Perez",
+    bio: "Ing. Industrial",
+    social: [
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/justme.fla/?hl=es",
+      },
+    ],
+  },
+  {
+    img: NatalyGemio,
+    name: "Nataly Alejandra Gemio Morales",
+    bio: "Estudiante de informatica en la Universidad Mayor De San Andres.",
+    social: [
+      {
+        social_img: logoFacebook,
+        src: "https://www.facebook.com/nataly.gemio/",
+      },
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/tugfanat/",
+      },
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/nataly-gemio-32602a216/",
+      },
+    ],
+  },
+  {
+    img: GabrielaTorrico,
+    name: "Gabriela Nathaly Torrico",
+    bio: "Agente de soporte técnico IT y Docente de Sistemas Informáticos.",
+    social: [
+      {
+        social_img: logoLinkedin,
+        src: "www.linkedin.com/in/gabriela-torrico-33925416a",
+      },
+    ],
+  },
+  {
+    img: AdrianaPaz,
+    name: "Adriana Eileen Paz Rivas",
+    bio: "Ing. Electromecánica y Profesora de violín.",
+    social: [
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/adri_gominola/"
+      },
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/adriana-eileen-paz-rivas/",
+      }
+    ]
+  },
+  {
+    img: JacquelineMejia,
+    name: "Jacqueline Susan Mejía",
+    bio: "Ingeniera de Sistemas / Data Analyst.",
+    social: [
+      {
+        social_img: logoFacebook,
+        src: "https://www.instagram.com/adri_gominola/"
+      },
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/susy.jam/"
+      },
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/susyjam/"
+      },
+      {
+        social_img: logoTwitter,
+        src: "https://twitter.com/susyjam",
+      }
+    ]
+  },
+  {
+    img: NataliaAlfaro,
+    name: "Natalia Lorena Alfaro Fernández",
+    bio: "Assuresoft - Senior Software Developer",
+    social: [
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/natalia-alfaro-dev?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+      }
+    ]
+  },
+  {
+    img: MariaGarcia,
+    name: "Maria Griselda Garcia Flores",
+    bio: "Jalasoft / Software engineer",
+    social: [
+      {
+        social_img: logoFacebook,
+        src: "https://www.facebook.com/combigriss"
+      },
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/combigriss/"
+      },
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/combigriss/"
+      },
+    ]
+  },
+  {
+    img: FrancisBanos,
+    name: "Francis Nicole Baños Flores",
+    bio: "Estudiante de Ingeniería de Sistemas.",
+    social: [
+      {
+        social_img: logoFacebook,
+        src: "https://www.facebook.com/nikita.francis.756?mibextid=ZbWKwL"
+      },
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/nickita.francis?utm_source=qr&igsh=bndzYmVsZnpob21j"
+      },
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/francis-nicole-ba%C3%B1os-flores-86a1942ba?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+      },
+      {
+        social_img: logoTwitter,
+        src: "https://x.com/nickitafrancis7?t=6LDICxGDEcSniIAH6x0G6g&s=09"
+      },
+      {
+        social_img: logoGithub,
+        src: "https://github.com/NikitaFrancis7"
+      },
+    ]
+  },
+  {
+    img: LupeMelany,
+    name: "Lupe Canaviri & Melany Ventura",
+    bio: "Estudiante de cine en la UMSA (Lupe), Organizer r00thouse HackLab La Paz (Melany).",
+    social: [
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/luucamay"
+      },
+      {
+        social_img: logoLinkedin,
+        src: "http://linkedin.com/in/luucamay/"
+      },
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/dev.ventura_/"
+      },
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/melany-ventura/"
+      }
+    ]
+  },
+  {
+    img: PedroAnze,
+    name: "Pedro Anze",
+    bio: "GDG Organizer, Web Developer, Estudiante de Ingeniería de Sistemas",
+    social: [
+      {
+        social_img: logoFacebook,
+        src: "https://www.facebook.com/profile.php?id=100014069383690"
+      },
+      {
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/pedroanze_/"
+      },
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/pedro-gustavo-anze-condori-65786a163/"
+      }
+    ]
+  },
+  {
+    img: CarliCode,
+    name: "Carli Code",
+    bio: "Head of AI at Dapta",
+    social: [
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/carlamfr/"
+      }
+    ]
+  },
+  {
+    img: MauricioArce,
+    name: "Mauricio Arce Torrez",
+    bio: "Jalasoft / Technical Lead / Ingeniero de Sistemas",
+    social: [
+      {
+        social_img: logoWeb,
+        src: "https://linktr.ee/combimauri"
+      }
+    ]
+  },
+  {
+    img: ReneCardozo,
+    name: "Rene Cardozo",
+    bio: "ONU",
+    social: [
+      {
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/rene-cardozo-ba56301a1"
+      }
+    ]
+  },
+  {
+    img: ElynMamani,
+    name: "Elyn Maya Mamani Mamani",
+    bio: "Cofundadora de la Academia de Programación y Robótica Educativa mi MakerHouse",
+    social: [
+      {
+        social_img: logoYoutube,
+        src: "https://www.youtube.com/@MakerMayita"
+      }
+    ]
+  },
+];
