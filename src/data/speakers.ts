01a6a9d335ce9e9ce94410094fa437105b23b9eb
Brian Dennis Vega Hidalgo <dentvega6@gmail.com>
2026-03-21 07:43:23 -0400

    speakers

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -1,19 +1,3 @@
-import CamilaVia from "../assets/images/speakers/camila-via.png";
-import FlaviaDavila from "../assets/images/speakers/flavia-davila.png";
-import NatalyGemio from "../assets/images/speakers/nataly-gemio.png";
-import GabrielaTorrico from "../assets/images/speakers/gabriela-torrico.png";
-import AdrianaPaz from "../assets/images/speakers/adriana-paz.png";
-import JacquelineMejia from "../assets/images/speakers/jacqueline-mejia.png";
-import NataliaAlfaro from "../assets/images/speakers/natalia-alfaro.png";
-import MariaGarcia from "../assets/images/speakers/maria-garcia.png";
-import FrancisBanos from "../assets/images/speakers/nicole-banos.png";
-import LupeMelany from "../assets/images/speakers/lupe-mel.png";
-import PedroAnze from "../assets/images/speakers/pedro-anze.png";
-import CarliCode from "../assets/images/speakers/carla-florida.png";
-import MauricioArce from "../assets/images/speakers/mauricio-arce.png";
-import ReneCardozo from "../assets/images/speakers/rene-cardozo.png";
-import ElynMamani from "../assets/images/speakers/elyn-mamani.png";
-
 import logoWeb from "../assets/images/social/web.svg";
 import logoLinkedin from "../assets/images/social/linkedin.svg";
 import logoFacebook from "../assets/images/social/facebook.svg";
@@ -22,287 +6,168 @@ import logoTwitter from "../assets/images/social/twitter.svg";
 import logoGithub from "../assets/images/social/github.svg";
 import logoYoutube from "../assets/images/social/youtube.svg";
 
+const drivePhoto = (id: string) =>
+  `https://drive.google.com/thumbnail?id=${id}&sz=w300`;
+
 export const speakers = [
   {
-    img: CamilaVia,
-    name: "Camila Flor Via",
-    speakerTopic: "El futuro de la IA Generativa en Latinoamérica",
-    bio: "CEO de Podcast Agarrate Catalina",
-    category: "Inspiracional",
-    time: "10:00 - 10:45",
+    img: drivePhoto("1Yx1rZTK0d8TOrog4cyrQFZh4iPl0l6Zt"),
+    name: "Maria Alejandra Mamani Cordero",
+    speakerTopic: "Enseñar a ser valientes, no perfectas",
+    bio: "Especialista en educación y tecnología, actualmente cursando una maestría en Seguridad Informática e IA.",
+    category: "Educación",
+    time: "09:00 - 09:45",
     social: [
       {
-        social_img: logoFacebook,
-        src: "https://www.facebook.com/Agarratecatalinabol",
-      },
-      {
-        social_img: logoInstagram,
-        src: "https://www.instagram.com/agarratecatalinabol?igsh=ZmU1ajI4MXFhOW0z",
+        social_img: logoLinkedin,
+        src: "https://linkedin.com/in/maria-alejandra-mamani-cordero-65628a12a",
       },
     ],
   },
   {
-    img: FlaviaDavila,
-    name: "Flavia Maylin Davila Perez",
-    speakerTopic: "Transformando industrias con IA",
-    bio: "Ing. Industrial",
-    category: "IA",
-    time: "10:00 - 10:45",
+    img: drivePhoto("1oWlTHNKWUgiPWyzkB6-ZyLG3O5oW8man"),
+    name: "Basilia Patzi Choqueticlla",
+    speakerTopic: "Más allá del baile",
+    bio: "Directora de Bhangare Dance Studio con 11 años de trayectoria formando bailarines en Cochabamba.",
+    category: "Cultura",
+    time: "09:00 - 10:00",
     social: [
       {
-        social_img: logoInstagram,
-        src: "https://www.instagram.com/justme.fla/?hl=es",
+        social_img: logoFacebook,
+        src: "https://www.facebook.com/share/1DFoBt8pHy/",
       },
     ],
   },
   {
-    img: NatalyGemio,
-    name: "Nataly Alejandra Gemio Morales",
-    speakerTopic: "Desarrollo web moderno y escalable",
-    bio: "Estudiante de informatica en la Universidad Mayor De San Andres.",
-    category: "Web",
-    time: "10:00 - 10:45",
+    img: drivePhoto("1udlzgIhnQaQ05rfdslCulORTgArxSyRw"),
+    name: "Odaliz Nicol",
+    speakerTopic:
+      "¿Los limites mentales y fisicos realmente existen cuando se trata de crecer?",
+    bio: "Ex-Presidenta Nacional de AIESEC en Bolivia, enfocada en bienestar personal y gestión del Burnout.",
+    category: "Storytelling",
+    time: "09:45 - 10:00",
     social: [
       {
-        social_img: logoFacebook,
-        src: "https://www.facebook.com/nataly.gemio/",
-      },
-      {
-        social_img: logoInstagram,
-        src: "https://www.instagram.com/tugfanat/",
-      },
-      {
         social_img: logoLinkedin,
-        src: "https://www.linkedin.com/in/nataly-gemio-32602a216/",
+        src: "https://www.linkedin.com/in/odaliz-rojas-roldan/",
       },
     ],
   },
   {
-    img: GabrielaTorrico,
-    name: "Gabriela Nathaly Torrico",
-    speakerTopic: "Comunidad tech: Creando espacios inclusivos",
-    bio: "Agente de soporte técnico IT y Docente de Sistemas Informáticos.",
-    category: "Comunidad",
+    img: drivePhoto("1ZATfcjQiaMSjZgo_tqaxG1_bOxzyhADk"),
+    name: "LILIANA VALENTINA LARRAZABAL",
+    speakerTopic:
+      "Romper patrones invisibles: salud mental y liderazgo femenino en tecnología",
+    bio: "Psicóloga clínica especializada en TCC y EMDR, enfocada en liderazgo sostenible y humano.",
+    category: "Salud Mental",
     time: "10:00 - 10:45",
     social: [
       {
-        social_img: logoLinkedin,
-        src: "www.linkedin.com/in/gabriela-torrico-33925416a",
+        social_img: logoInstagram,
+        src: "https://www.instagram.com/psico__valentina",
       },
     ],
   },
   {
-    img: AdrianaPaz,
-    name: "Adriana Eileen Paz Rivas",
-    speakerTopic: "Rompiendo barreras en STEM",
-    bio: "Ing. Electromecánica y Profesora de violín.",
-    category: "Inspiracional",
-    time: "10:00 - 10:45",
-    social: [
-      {
-        social_img: logoInstagram,
-        src: "https://www.instagram.com/adri_gominola/"
-      },
-      {
-        social_img: logoLinkedin,
-        src: "https://www.linkedin.com/in/adriana-eileen-paz-rivas/",
-      }
-    ]
-  },
-  {
-    img: JacquelineMejia,
-    name: "Jacqueline Susan Mejía",
-    speakerTopic: "Data Analytics: Del análisis a la acción",
-    bio: "Ingeniera de Sistemas / Data Analyst.",
-    category: "IA",
-    time: "10:00 - 10:45",
-    social: [
-      {
-        social_img: logoFacebook,
-        src: "https://www.instagram.com/adri_gominola/"
-      },
-      {
-        social_img: logoInstagram,
-        src: "https://www.instagram.com/susy.jam/"
-      },
-      {
-        social_img: logoLinkedin,
-        src: "https://www.linkedin.com/in/susyjam/"
-      },
-      {
-        social_img: logoTwitter,
-        src: "https://twitter.com/susyjam",
-      }
-    ]
-  },
-  {
-    img: NataliaAlfaro,
-    name: "Natalia Lorena Alfaro Fernández",
-    speakerTopic: "Arquitectura de software en proyectos grandes",
-    bio: "Assuresoft - Senior Software Developer",
-    category: "Web",
-    time: "10:00 - 10:45",
+    img: drivePhoto("12uD-hIRGaVaSiju_dB6IU1c9imtPqylb"),
+    name: "Raquel Andrea Calderón Gallardo",
+    speakerTopic: "Crónicas de un Salto al Vacío",
+    bio: "Ingeniera biomédica apasionada por la IA aplicada a la salud y neurotecnología.",
+    category: "Desarrollo Personal",
+    time: "10:45 - 11:00",
     social: [
       {
         social_img: logoLinkedin,
-        src: "https://www.linkedin.com/in/natalia-alfaro-dev?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-      }
-    ]
+        src: "https://www.linkedin.com/in/raquel-andrea-calderon-gallardo/",
+      },
+    ],
   },
   {
-    img: MariaGarcia,
-    name: "Maria Griselda Garcia Flores",
-    speakerTopic: "Desarrollo fullstack con tecnologías modernas",
-    bio: "Jalasoft / Software engineer",
-    category: "Web",
-    time: "10:00 - 10:45",
+    img: drivePhoto("1PWdiymDgCB6HgAjBDjkTzXulpdn6fRhe"),
+    name: "Diandra Nathaly Céspedes Sagardía",
+    speakerTopic:
+      "Violencia digital de género: una realidad pendiente de abordar",
+    bio: "Abogada especialista en derechos digitales y defensa de los derechos humanos.",
+    category: "Derechos Digitales",
+    time: "11:00 - 11:15",
     social: [
-      {
-        social_img: logoFacebook,
-        src: "https://www.facebook.com/combigriss"
-      },
-      {
-        social_img: logoInstagram,
-        src: "https://www.instagram.com/combigriss/"
-      },
-      {
-        social_img: logoLinkedin,
-        src: "https://www.linkedin.com/in/combigriss/"
-      },
-    ]
+      { social_img: logoWeb, src: "http://linktr.ee/diandra.n.cespedes.s" },
+    ],
   },
   {
-    img: FrancisBanos,
-    name: "Francis Nicole Baños Flores",
-    speakerTopic: "Iniciándose en ingeniería de sistemas",
-    bio: "Estudiante de Ingeniería de Sistemas.",
-    category: "Comunidad",
-    time: "10:00 - 10:45",
-    social: [
-      {
-        social_img: logoFacebook,
-        src: "https://www.facebook.com/nikita.francis.756?mibextid=ZbWKwL"
-      },
-      {
-        social_img: logoInstagram,
-        src: "https://www.instagram.com/nickita.francis?utm_source=qr&igsh=bndzYmVsZnpob21j"
-      },
-      {
-        social_img: logoLinkedin,
-        src: "https://www.linkedin.com/in/francis-nicole-ba%C3%B1os-flores-86a1942ba?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-      },
-      {
-        social_img: logoTwitter,
-        src: "https://x.com/nickitafrancis7?t=6LDICxGDEcSniIAH6x0G6g&s=09"
-      },
-      {
-        social_img: logoGithub,
-        src: "https://github.com/NikitaFrancis7"
-      },
-    ]
-  },
-  {
-    img: LupeMelany,
-    name: "Lupe Canaviri & Melany Ventura",
-    speakerTopic: "Comunidades tech y hackathons en Bolivia",
-    bio: "Estudiante de cine en la UMSA (Lupe), Organizer r00thouse HackLab La Paz (Melany).",
-    category: "Comunidad",
-    time: "10:00 - 10:45",
-    social: [
-      {
-        social_img: logoInstagram,
-        src: "https://www.instagram.com/luucamay"
-      },
-      {
-        social_img: logoLinkedin,
-        src: "http://linkedin.com/in/luucamay/"
-      },
-      {
-        social_img: logoInstagram,
-        src: "https://www.instagram.com/dev.ventura_/"
-      },
-      {
-        social_img: logoLinkedin,
-        src: "https://www.linkedin.com/in/melany-ventura/"
-      }
-    ]
-  },
-  {
-    img: PedroAnze,
-    name: "Pedro Anze",
-    speakerTopic: "Desarrollo web y comunidad open source",
-    bio: "GDG Organizer, Web Developer, Estudiante de Ingeniería de Sistemas",
-    category: "Web",
-    time: "10:00 - 10:45",
-    social: [
-      {
-        social_img: logoFacebook,
-        src: "https://www.facebook.com/profile.php?id=100014069383690"
-      },
-      {
-        social_img: logoInstagram,
-        src: "https://www.instagram.com/pedroanze_/"
-      },
-      {
-        social_img: logoLinkedin,
-        src: "https://www.linkedin.com/in/pedro-gustavo-anze-condori-65786a163/"
-      }
-    ]
-  },
-  {
-    img: CarliCode,
-    name: "Carli Code",
-    speakerTopic: "IA generativa: Aplicaciones prácticas",
-    bio: "Head of AI at Dapta",
-    category: "IA",
-    time: "10:00 - 10:45",
+    img: drivePhoto("18fSUA6tLk2cYLZUA7p_e1cDyR_gBK-Tp"),
+    name: "Gaston Joshua Zientarski Rivero",
+    speakerTopic: "Inteligencia emocional para equipos de trabajo",
+    bio: "Licenciado en Ciencias Jurídicas y activista en la construcción de espacios juveniles e incidencia pública.",
+    category: "Liderazgo",
+    time: "11:00 - 12:00",
     social: [
       {
         social_img: logoLinkedin,
-        src: "https://www.linkedin.com/in/carlamfr/"
-      }
-    ]
+        src: "https://www.linkedin.com/in/gzientarski/",
+      },
+    ],
   },
   {
-    img: MauricioArce,
-    name: "Mauricio Arce Torrez",
-    speakerTopic: "Diseño UX/UI en la era digital",
-    bio: "Jalasoft / Technical Lead / Ingeniero de Sistemas",
-    category: "Web",
-    time: "10:00 - 10:45",
-    social: [
-      {
-        social_img: logoWeb,
-        src: "https://linktr.ee/combimauri"
-      }
-    ]
-  },
-  {
-    img: ReneCardozo,
-    name: "Rene Cardozo",
-    speakerTopic: "Sostenibilidad y tecnología",
-    bio: "ONU",
-    category: "Inspiracional",
-    time: "10:00 - 10:45",
+    img: drivePhoto("1dzIzQCcemMQY3mVsffCSqY4t-LVRUeq8"),
+    name: "Nicole Cartagena Siles",
+    speakerTopic:
+      "Tu carrera no es una caja: crecer profesionalmente en un área sin muchos referentes",
+    bio: "Ingeniera biomédica con experiencia en salud, tecnología y educación.",
+    category: "Trayectoria",
+    time: "11:15 - 12:00",
     social: [
       {
         social_img: logoLinkedin,
-        src: "https://www.linkedin.com/in/rene-cardozo-ba56301a1"
-      }
-    ]
+        src: "https://www.linkedin.com/in/nicole-cartagena-siles-52ab92263",
+      },
+    ],
   },
   {
-    img: ElynMamani,
-    name: "Elyn Maya Mamani Mamani",
-    speakerTopic: "Educación en programación y robótica",
-    bio: "Cofundadora de la Academia de Programación y Robótica Educativa mi MakerHouse",
-    category: "Comunidad",
-    time: "10:00 - 10:45",
+    img: drivePhoto("1cJTStmrdr9wB6NELVbPY3-sis7SBvkQe"),
+    name: "Dafne Alejandra Caballero",
+    speakerTopic:
+      "Cómo la inteligencia artificial potencia las campañas de pago",
+    bio: "Especialista en IA aplicada al marketing digital y optimización de campañas de paid media.",
+    category: "Marketing / IA",
+    time: "13:20 - 13:35",
     social: [
       {
-        social_img: logoYoutube,
-        src: "https://www.youtube.com/@MakerMayita"
-      }
-    ]
+        social_img: logoLinkedin,
+        src: "https://www.linkedin.com/in/dafnealejandracaballerocarrasco/",
+      },
+    ],
+  },
+  {
+    img: drivePhoto("1q0dS7_BygNzRzmPLefuJwewWuDbrb_0l"),
+    name: "María René Torrez Vargas",
+    speakerTopic: "Technovation: 10 años rompiendo paradigmas.",
+    bio: "Ingeniera de Sistemas, WTM Ambassador y Trainer de Cisco apasionada por la robótica educativa.",
+    category: "Educación",
+    time: "13:35 - 13:50",
+    social: [
+      { social_img: logoInstagram, src: "https://www.instagram.com/solorene" },
+    ],
+  },
+  {
+    img: drivePhoto("1g8_p58fdNXzYWIfNWdEhisU_KXhyjQhg"),
+    name: "Carlos Collazos",
+    speakerTopic:
+      "Trabajar remoto desde Bolivia para equipos internacionales suena increible",
+    bio: "Senior UX Designer en SportsEngine y Organizer de GDG Cochabamba.",
+    category: "Trabajo Remoto",
+    time: "13:50 - 14:20",
+    social: [
+      { social_img: logoInstagram, src: "https://instagram.com/ux.carlos" },
+    ],
+  },
+  {
+    img: drivePhoto("1Crm9Xd3V3OYsMDkSEXAqIk7I6HR7460q"),
+    name: "Anahí Sanabria Ugarte",
+    speakerTopic: "La programación competitiva puede cambiar tu carrera",
+    bio: "Backend Developer y competidora ICPC apasionada por la resolución de problemas algorítmicos.",
+    category: "Programación",
+    time: "14:20 - 14:35",
+    social: [{ social_img: logoGithub, src: "https://github.com/AnahiSU" }],
   },
 ];
